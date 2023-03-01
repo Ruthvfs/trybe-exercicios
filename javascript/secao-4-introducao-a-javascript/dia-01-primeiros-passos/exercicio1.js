3deb7d9fb5fccb58ecb8c19208dd04aefa1b34d4
Ruthvfs <fruth915@gmail.com>
2023-03-01 00:32:14 -0300

    segunda e terceira

--- javascript/secao-4-introducao-a-javascript/dia-01-primeiros-passos/exercicio1.js
+++ javascript/secao-4-introducao-a-javascript/dia-01-primeiros-passos/exercicio1.js
@@ -1,8 +1,31 @@
-//primeira questao
-let a = 2;
-let b = 8;
-console.log('soma: ' + (a+b));
-console.log('subtração: '+(a-b));
-console.log('multiplicação: '+(a*b));
-console.log('divisão: '+ (a/b));
-console.log('módulo: '+(a%b));
+//primeira questao:
+//let a = 2;
+//let b = 8;
+//console.log('soma: ' + (a+b));
+//console.log('subtração: '+(a-b));
+//console.log('multiplicação: '+(a*b));
+//console.log('divisão: '+ (a/b));
+//console.log('módulo: '+(a%b));//
+
+//segunda questão:
+let number1 = 150;
+let number2 = 80;
+
+if(number1 > number2){
+    console.log(number1 +' é maior que '+ number2);
+}else{
+    console.log(number2 + ' é maior que '+ number1);
+}
+
+//terceira questão
+let a = 22;
+let b = 14;
+let c = 5;
+
+if(a >b && a >c ){
+    console.log('o maior número é: ' + a + '(a)');
+}else if(b > a && b > c){
+    console.log('o maior número é: ' + b+ '(b)');
+}else{
+    console.log('o maior número é: '+ c+ '(c)');
+}
