6f01b8dba6096d2ae940ac0ee2f6d77f326bb130
Ruthvfs <fruth915@gmail.com>
2023-03-01 00:12:40 -0300

    primeira questão

---
+++ javascript/secao-4-introducao-a-javascript/dia-01-primeiros-passos/exercicio1.js
@@ -0,0 +1,8 @@
+//primeira questao
+let a = 2;
+let b = 8;
+console.log('soma: ' + (a+b));
+console.log('subtração: '+(a-b));
+console.log('multiplicação: '+(a*b));
+console.log('divisão: '+ (a/b));
+console.log('módulo: '+(a%b));
